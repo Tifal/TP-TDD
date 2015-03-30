43b431b2373bf5e498ae74f8364f630588b531d2
Raphaël Fitoussi <fitoussr@saule07.iut-infobio.priv.univ-lille1.fr>
2015-03-30 15:32:18 +0200

    TP-TDDv2.5

--- src/test/java/iut/tdd/TestConvert.java
+++ src/test/java/iut/tdd/TestConvert.java
@@ -1,68 +1,97 @@
 package iut.tdd;
 
-import org.junit.Test; 
-import org.junit.Assert; 
+import org.junit.Test;
+import org.junit.Assert;
 
 public class TestConvert {
+	static Convert myConvert = new Convert();
+
 	@Test
-	public void test_num2text_zero () {
-		// Given
-		Convert myConvert = new Convert();
+	public void test_num2text_zero() {
 		Assert.assertEquals("zéro", myConvert.num2text("0"));
-		// When
-		
-	// Then
-	}
-//Given
-	@Test
-	public void test_num2text_un (){
-		 Convert myConvert = new Convert();
-		Assert.assertEquals("un", myConvert.num2text("1"));
-		// When
-		
-	// Then
+
 	}
 
-//Given
 	@Test
-		public void test_num2text_de2a16 (){
-			
-		 Convert myConvert = new Convert();
+	public void test_num2text_un() {
+		Assert.assertEquals("un", myConvert.num2text("1"));
+
+	}
+
+	@Test
+	public void test_num2text_de2a16() {
 		Assert.assertEquals("deux", myConvert.num2text("2"));
-		// When
-		
-	// Then
-		//Given
-		myConvert = new Convert();
+
+	}
+
+	@Test
+	public void test_num2text_3() {
 		Assert.assertEquals("trois", myConvert.num2text("3"));
-				// When
-				
-			// Then
-		myConvert = new Convert();
+
+	}
+
+	@Test
+	public void test_num2text_4() {
 		Assert.assertEquals("quatre", myConvert.num2text("4"));
-		myConvert = new Convert();
+	}
+
+	@Test
+	public void test_num2text_5() {
 		Assert.assertEquals("cinq", myConvert.num2text("5"));
-		myConvert = new Convert();
+	}
+
+	@Test
+	public void test_num2text_6() {
 		Assert.assertEquals("six", myConvert.num2text("6"));
-		myConvert = new Convert();
+	}
+
+	@Test
+	public void test_num2text_7() {
 		Assert.assertEquals("sept", myConvert.num2text("7"));
-		myConvert = new Convert();
+	}
+
+	@Test
+	public void test_num2text_8() {
 		Assert.assertEquals("huit", myConvert.num2text("8"));
-		myConvert = new Convert();
+	}
+
+	@Test
+	public void test_num2text_9() {
 		Assert.assertEquals("neuf", myConvert.num2text("9"));
-		myConvert = new Convert();
+	}
+
+	@Test
+	public void test_num2text_10() {
 		Assert.assertEquals("dix", myConvert.num2text("10"));
-		myConvert = new Convert();
+	}
+
+	@Test
+	public void test_num2text_11() {
 		Assert.assertEquals("onze", myConvert.num2text("11"));
-		myConvert = new Convert();
+	}
+
+	@Test
+	public void test_num2text_12() {
 		Assert.assertEquals("douze", myConvert.num2text("12"));
-		myConvert = new Convert();
+	}
+
+	@Test
+	public void test_num2text_13() {
 		Assert.assertEquals("treize", myConvert.num2text("13"));
-		myConvert = new Convert();
+	}
+
+	@Test
+	public void test_num2text_14() {
 		Assert.assertEquals("quatorze", myConvert.num2text("14"));
-		myConvert = new Convert();
+	}
+
+	@Test
+	public void test_num2text_15() {
 		Assert.assertEquals("quinze", myConvert.num2text("15"));
-		myConvert = new Convert();
-		Assert.assertEquals("seize", myConvert.num2text("16"));}}
-		
-	
+	}
+
+	@Test
+	public void test_num2text_16() {
+		Assert.assertEquals("seize", myConvert.num2text("16"));
+	}
+}
